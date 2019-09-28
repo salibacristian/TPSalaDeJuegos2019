0f0efda151b070ce24f139d6d48fdf34e30d5c97
salibacristian <chriseze21@gmail.com>
2019-09-28 18:37:45 -0300

    init app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,80 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { AdivinaElNumeroComponent } from './componentes/adivina-el-numero/adivina-el-numero.component';
+import { ListadoDeResultadosComponent } from './componentes/listado-de-resultados/listado-de-resultados.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { HttpModule } from '@angular/http';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { MiHttpService } from './servicios/mi-http/mi-http.service'; 
+import { PaisesService } from './servicios/paises.service'; 
+
+import { JugadoresService } from './servicios/jugadores.service'; 
+import{ ArchivosJugadoresService} from './servicios/archivos-jugadores.service'; 
+import { ErrorComponent } from './componentes/error/error.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
+import { AgilidadAritmeticaComponent } from './componentes/agilidad-aritmetica/agilidad-aritmetica.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { AdivinaMasListadoComponent } from './componentes/adivina-mas-listado/adivina-mas-listado.component';
+import { AgilidadMasListadoComponent } from './componentes/agilidad-mas-listado/agilidad-mas-listado.component';
+import { ListadoComponent } from './componentes/listado/listado.component';
+
+import { JugadoresListadoComponent } from './componentes/jugadores-listado/jugadores-listado.component';
+
+import { JuegoServiceService } from './servicios/juego-service.service';
+import { ListadosComponent } from './componentes/listados/listados.component';
+import { JuegosComponent } from './componentes/juegos/juegos.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { MenuCardComponent } from './componentes/menu-card/menu-card.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { AnagramaComponent } from './componentes/anagrama/anagrama.component';
+import { ListadoDePaisesComponent } from './componentes/listado-de-paises/listado-de-paises.component';
+import { MapaDeGoogleComponent } from './componentes/mapa-de-google/mapa-de-google.component'
+import { AgmCoreModule } from '@agm/core';
+import { InputJugadoresComponent } from './componentes/input-jugadores/input-jugadores.component';
+import { SexoPipe } from './pipes/sexo.pipe';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdivinaElNumeroComponent,
+    ListadoDeResultadosComponent,
+    ErrorComponent,
+    PrincipalComponent,
+    LoginComponent,
+    AgilidadAritmeticaComponent,
+    MenuComponent,
+    AdivinaMasListadoComponent,
+    AgilidadMasListadoComponent,
+    ListadoComponent,
+    ListadosComponent,
+    JuegosComponent,
+    RegistroComponent,
+    MenuCardComponent,
+    CabeceraComponent,
+    QuienSoyComponent,
+    AnagramaComponent,
+    ListadoDePaisesComponent,
+    MapaDeGoogleComponent,
+    JugadoresListadoComponent,
+    InputJugadoresComponent,
+    SexoPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB6f8x4IjRlesQ3oETc6BXYQHVRTOlY3Ys'
+    })
   ],
-  providers: [],
+  providers: [JuegoServiceService, MiHttpService,PaisesService,ArchivosJugadoresService,JugadoresService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
